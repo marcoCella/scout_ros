11bfc2618fc00583d340452ef8d54e27f2027b5f
Marco Cella <marco.cella@skywarder.eu>
2021-04-29 19:35:38 +0200

    Added a launch file that also runs the bag and rviz

--- project1/src/residuals.cpp
+++ project1/src/residuals.cpp
@@ -1,13 +1,13 @@
 #include "ros/ros.h"
-#include "nav_msgs/Odometry.h" 
-#include "project1/er_array.h" 
-#include <tf/tf.h> 
+#include "nav_msgs/Odometry.h"
+#include "project1/er_array.h"
+#include <tf/tf.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
-using OD  = nav_msgs::Odometry;
+using OD = nav_msgs::Odometry;
 
 class residual
 
@@ -15,28 +15,24 @@ class residual
 private:
     ros::NodeHandle n;
 
-    
     project1::er_array error_array;
     message_filters::Subscriber<OD> our_odom_sub;
     message_filters::Subscriber<OD> scout_odom_sub;
-    
 
     typedef message_filters::sync_policies::ApproximateTime<OD, OD> SyncPolicy_er;
     typedef message_filters::Synchronizer<SyncPolicy_er> Sync_er;
     boost::shared_ptr<Sync_er> sync_er;
 
-
 public:
-
     float dx, dy, dtheta, dtot;
     ros::Publisher pub_er = n.advertise<project1::er_array>("/residual", 100);
-    ros::Subscriber re_timer;      //listen to scout odom, republih the message with a new timestamp
-    ros::Publisher mirror; // publish the restamped message for the filter to synchronize
+    ros::Subscriber re_timer; //listen to scout odom, republih the message with a new timestamp
+    ros::Publisher mirror;    // publish the restamped message for the filter to synchronize
 
     residual()
     {
-        re_timer = n.subscribe("/scout_odom",  100, &residual::time_callback, this);
-        
+        re_timer = n.subscribe("/scout_odom", 100, &residual::time_callback, this);
+
         scout_odom_sub.subscribe(n, "/real_time_odom", 100);
         our_odom_sub.subscribe(n, "/our_odom", 100);
 
@@ -44,21 +40,19 @@ public:
         sync_er->registerCallback(boost::bind(&residual::callback_er, this, _1, _2));
     }
 
-    void time_callback(const OD::ConstPtr& scout_odom)
+    void time_callback(const OD::ConstPtr &scout_odom)
     {
         mirror = n.advertise<OD>("/real_time_odom", 100);
         OD real_time_odom;
         real_time_odom = *scout_odom;
-        real_time_odom.header.stamp = ros::Time::now();  //real time is the same as scout but with header "now"
+        real_time_odom.header.stamp = ros::Time::now(); //real time is the same as scout but with header "now"
 
         mirror.publish(real_time_odom);
     }
-    
+
     //this recieve the odom from the bag, with the corrected time stamp, and the one from our odompubsub
-    void callback_er(const OD::ConstPtr& scout_odom, const OD::ConstPtr& our_odom)   
+    void callback_er(const OD::ConstPtr &scout_odom, const OD::ConstPtr &our_odom)
     {
-        ROS_INFO("I'm residuating");
-        
         //quaternion to RPY
         tf::Quaternion scout(
             scout_odom->pose.pose.orientation.x,
@@ -81,7 +75,7 @@ public:
         float dx = our_odom->pose.pose.position.x - scout_odom->pose.pose.position.x;
         float dy = our_odom->pose.pose.position.y - scout_odom->pose.pose.position.y;
         float dtheta = yaw_our - yaw_scout;
-        float dtot = sqrt(pow(dx, 2.0f)+pow(dy, 2.0f));
+        float dtot = sqrt(pow(dx, 2.0f) + pow(dy, 2.0f));
 
         error_array.dx.data = dx;
         error_array.dy.data = dy;
@@ -89,13 +83,8 @@ public:
         error_array.dtot.data = dtot;
 
         pub_er.publish(error_array);
-        
     }
 };
-
-
-
-
 
 int main(int argc, char **argv)
 {
@@ -104,7 +93,5 @@ int main(int argc, char **argv)
     residual err;
     ros::spin();
 
-    return 0;  
-    
+    return 0;
 }
-
